c62297a678c3c0fe728ad2589f383b350e190a5d
Shivanshu Singh <shivanshusingh42005@gmail.com>
2026-06-04 20:59:41 +0530

    Implement matrixReshape function in ReshapeTheMatrix.cpp

---
+++ Week_1/Day_4/ReshapeTheMatrix.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        
+        int m = mat.size();
+        int n = mat[0].size();
+
+        
+        if(m * n != r * c) {
+            return mat;
+        }
+
+        vector<vector<int>> res(r, vector<int>(c));
+
+        int x = 0;
+        int y = 0;
+
+        for(int i = 0; i < m; i++) {
+
+            for(int j = 0; j < n; j++) {
+
+                res[x][y] = mat[i][j];
+                y++;
+
+              
+                if(y == c) {
+                    y = 0;
+                    x++;
+                }
+            }
+        }
+
+        return res;
+    }
+};
