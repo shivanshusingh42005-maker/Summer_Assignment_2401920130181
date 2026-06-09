405bca738aaca463579f803b60e4bf66fe6b616b
Shivanshu Singh <shivanshusingh42005@gmail.com>
2026-06-09 22:12:37 +0530

    Implement permutation check in a string

---
+++ Week_2/Day_2/PermutationInAString.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+
+        if(s1.size() > s2.size()) {
+            return false;
+        }
+
+        sort(s1.begin(), s1.end());
+
+        for(int i = 0; i <= s2.size() - s1.size(); i++) {
+
+            string t = s2.substr(i, s1.size());
+
+            sort(t.begin(), t.end());
+
+            if(t == s1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
