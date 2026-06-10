65127e70a324c09c71d8a3978d675dce7390fc5b
Shivanshu Singh <shivanshusingh42005@gmail.com>
2026-06-10 21:58:56 +0530

    Implement repeatedSubstringPattern function

---
+++ Week_2/Day_3/RepeatSubstring.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+
+        int n = s.size();
+
+        for (int len = 1; len <= n / 2; len++) {
+
+            if (n % len == 0) {
+
+                string temp = "";
+
+                for (int i = 0; i < n / len; i++) {
+                    temp += s.substr(0, len);
+                }
+
+                if (temp == s) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
