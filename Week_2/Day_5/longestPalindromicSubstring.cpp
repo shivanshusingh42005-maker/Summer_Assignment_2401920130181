a2b4281921c620c26093cb8852c8853578640743
Shivanshu Singh <shivanshusingh42005@gmail.com>
2026-06-12 22:04:45 +0530

    Implement longest palindromic substring function

---
+++ Week_2/Day_5/longestPalindromicSubstring.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string res = "";
+
+        for (int i = 0; i < s.size(); i++) {
+            int l = i, r = i;
+
+            
+            while (l >= 0 && r < s.size() && s[l] == s[r]) {
+                if (r - l + 1 > res.size()) {
+                    res = s.substr(l, r - l + 1);
+                }
+                l--;
+                r++;
+            }
+
+            l = i;
+            r = i + 1;
+
+            
+            while (l >= 0 && r < s.size() && s[l] == s[r]) {
+                if (r - l + 1 > res.size()) {
+                    res = s.substr(l, r - l + 1);
+                }
+                l--;
+                r++;
+            }
+        }
+
+        return res;
+    }
+};
